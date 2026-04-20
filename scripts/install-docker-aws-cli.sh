d6cf78321786b575cb40d332f3a9dedce59aa510
Jitendra Kumar <jkpatelnx@gmail.com>
2026-04-20 06:22:23 +0530

    Add installation script for Docker and AWS CLI
    This script installs Docker, Docker Compose, and the AWS CLI on a Linux system. It also adds the current user to the Docker group and reboots the system.

---
+++ scripts/install-docker-aws-cli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install docker 
+sudo apt update -y 
+sudo apt install -y docker.io docker-compose-v2 
+sudo usermod -aG docker ${SUDO_USER:-$USER}"
+
+# install aws cli
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo reboot 
