45e3cdb181edeb9af2c410de885735d8a124e377
Jitendra Kumar <jkpatelnx@gmail.com>
2026-04-20 05:44:51 +0530

    Enhance install-docker.sh to manage Docker service
    Added logic to stop and start the Docker service if the runner directory exists.

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
 set -euo pipefail
+
+RUNNER_DIR="/home/ubuntu/actions-runner"
 
 # Detect real user (works with sudo OR direct run)
 RUNNER_USER="${SUDO_USER:-$USER}"
@@ -10,10 +13,15 @@ if command -v docker >/dev/null 2>&1; then
 else
   sudo apt update -y
   sudo apt install -y docker.io docker-compose-v2
+  sudo usermod -aG docker "$RUNNER_USER" && newgrp docker
   echo "Docker installed."
 fi
 
-sudo usermod -aG docker "$RUNNER_USER" && newgrp docker
+if [ -d "$RUNNER_DIR" ]; then
+  cd "$RUNNER_DIR"
+  sudo ./svc.sh stop || true
+  sudo ./svc.sh start
+fi
 
 echo "######### Verifying installation #########"
 docker --version
