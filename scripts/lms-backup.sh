ba3ecbaede89e10b310b11d0ba7c8d63f29a434b
Jitendra Kumar <jkpatelnx@gmail.com>
2026-04-21 18:11:12 +0530

    Update cron job path in lms-backup.sh

--- scripts/lms-backup.sh
+++ scripts/lms-backup.sh
@@ -11,9 +11,9 @@ else
 fi
 
 
-#CRON_JOB='*/5 * * * * /home/ubuntu/actions-runner/_work/devsecops-lms-deployment/devsecops-lms-deployment/scripts/lms-backup.sh >> /home/ubuntu/backup.log 2>&1'
-CRON_JOB='*/5 * * * * /home/ubuntu/actions-runner/_work/devsecops-lms-deployment/devsecops-lms-deployment/scripts/lms-backup.sh'
-if sudo crontab -l 2>/dev/null | grep -Fq "/home/ubuntu/lms-backup.sh"; then
+#CRON_JOB='*/5 * * * * /home/ubuntu/devsecops-lms-deployment/scripts/lms-backup.sh >> /home/ubuntu/backup.log 2>&1'
+CRON_JOB='*/5 * * * * /home/ubuntu/devsecops-lms-deployment/scripts/lms-backup.sh'
+if sudo crontab -l 2>/dev/null | grep -Fq "/home/ubuntu/devsecops-lms-deployment/scripts/lms-backup.sh"; then
   echo "Cron job already exists, skipping..."
 else
   echo "Adding cron job..."
